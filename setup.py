31ba395d7090640d912014c588dc578f081e7f3f
dailin.ji <dailinji1212@gmail.com>
2022-10-09 16:34:46 -0400

    PyPi

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     author='Dailin Ji',
     author_email='dj2194@nyu.edu',
     # Needed to actually package something
-    packages=['Backtest'],
+    packages=['GeneralBacktest'],
     # Needed for dependencies
     install_requires=['numpy','pandas','pydrive','oauth2client'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
     license='SUPR',
-    description='A simplt, realistic tool to backtest trading strategies.',
+    description='A simple, realistic tool to backtest trading strategies.',
     long_description=open('README.txt').read(),
 )
